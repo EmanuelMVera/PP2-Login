49fda6192093c415c72873a1a4c7d5492db0b2fc
Emanuel <61530170+EmanuelMVera@users.noreply.github.com>
2023-10-27 07:25:28 -0300

    Update login-process.php
    Comentarios

--- process/login-process.php
+++ process/login-process.php
@@ -1,26 +1,38 @@
 <?php
+// Incluye el archivo de base de datos
 include_once("../database/database.php");
+// Inicia la sesión
 session_start();
 
+// Verifica si la solicitud es de tipo POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtiene y limpia el correo del usuario
     $mail = mysqli_real_escape_string($conn, trim($_POST["user"]));
+    // Obtiene la contraseña sin procesar
     $contrasena = trim($_POST["password"]);
 
+    // Consulta para buscar al usuario por correo
     $query = "SELECT * FROM usuarios WHERE mail = ?";
     $stmt = mysqli_prepare($conn, $query);
+    // Vincula el valor del correo al parámetro en la consulta
     mysqli_stmt_bind_param($stmt, "s", $mail);
+    // Ejecuta la consulta preparada
     mysqli_stmt_execute($stmt);
+    // Obtiene el resultado de la consulta
     $result = mysqli_stmt_get_result($stmt);
 
     if ($result && mysqli_num_rows($result) == 1) {
+        // Si se encuentra un usuario con el correo proporcionado
         $row = mysqli_fetch_assoc($result);
+        // Verifica si la contraseña ingresada coincide con la almacenada en la base de datos
         if (password_verify($contrasena, $row['contrasena'])) {
-            // Almacenar los datos del usuario en la sesión
+            // Almacena los datos del usuario en la sesión
             $_SESSION["idUsuario"] = $row['id'];
             $_SESSION["nombreUsuario"] = $row['nombre'];
             $_SESSION["apellidoUsuario"] = $row['apellido'];
             $_SESSION["correoUsuario"] = $row['mail'];
 
+            // Redirige al usuario a la página de inicio del dashboard
             header("Location: ../pages/dashboard.php");
             exit();
         } else {
