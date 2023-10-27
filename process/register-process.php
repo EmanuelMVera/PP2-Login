1aa5c826f772dbbb54e6655c30a295630b5462f6
Emanuel <61530170+EmanuelMVera@users.noreply.github.com>
2023-10-27 07:30:24 -0300

    Update register-process.php
    Comentarios

--- process/register-process.php
+++ process/register-process.php
@@ -1,22 +1,28 @@
 <?php
+// Incluye el archivo de la base de datos
 include_once("../database/database.php");
+// Inicia la sesión
 session_start();
 
+// Verifica si la solicitud es de tipo POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtiene y limpia los datos del formulario
     $nombre = mysqli_real_escape_string($conn, trim($_POST["nombre"]));
     $apellido = mysqli_real_escape_string($conn, trim($_POST["apellido"]));
     $correo = mysqli_real_escape_string($conn, trim($_POST["correo"]));
+    // Genera un hash de contraseña segura
     $contrasena = password_hash(trim($_POST["contrasena"]), PASSWORD_DEFAULT);
 
     // Validación de datos
     $error = false;
 
+    // Verifica si el correo es válido
     if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
         $error = true;
         $_SESSION["registerError"] = 'Correo electrónico no válido';
     }
 
-    // Verificar si el correo ya está registrado
+    // Verifica si el correo ya está registrado
     $query = "SELECT id FROM usuarios WHERE mail = ?";
     $stmt = mysqli_prepare($conn, $query);
     mysqli_stmt_bind_param($stmt, "s", $correo);
@@ -29,18 +35,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if ($error) {
-        // Redirige a register.php en caso de error
+        // Redirige a la página de registro en caso de error
         header("Location: ../register.php");
         exit();
     }
 
-    // Inserción de usuario
+    // Inserción de usuario en la base de datos
     $query = "INSERT INTO usuarios (nombre, apellido, mail, contrasena) VALUES (?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $query);
     mysqli_stmt_bind_param($stmt, "ssss", $nombre, $apellido, $correo, $contrasena);
 
     if (mysqli_stmt_execute($stmt)) {
-        // Registro exitoso, redirige a la página de inicio de sesión con mensaje de éxito
+        // Registro exitoso, redirige a la página de registro con mensaje de éxito
         $_SESSION["registrationSuccess"] = 'Registro exitoso!!!';
         header("Location: ../register.php");
         exit();
